1dc86250f6cbc7f9e06831211498589cd01081e4
Bruno <brunoncarvalho@outlook.com>
2018-03-01 00:56:18 -0300

    Filtragem quase concluida

--- filter.py
+++ filter.py
@@ -1,16 +1,25 @@
 import os
-import glob
-import csv
+# import glob
+# import csv
 import pywt
 import func
-import numpy as np
-import matplotlib.pyplot as plt
+# import gc
+# import numpy as np
+# import matplotlib.pyplot as plt
 from scipy import signal
 # from pyAudioAnalysis import audioFeatureExtraction as aF
 # import heartbeat as hb
 # import pandas as pd
-# from scipy.io import wavfile
+from scipy.io import wavfile
 # from math import isnan
+
+'''
+A ideia e ler cada faia de audio e filtrar:
+    carregar arquivo na memoria
+    fazer a filtragem com wavelet
+    escrever o vetor no .csv
+limpar a memoria para testar se nao trava
+'''
 
 # dataset PASCAL B
 btraning_normal = '/home/bruno/Documentos/UFMA/mono/dataset/B/Training B Normal'
@@ -41,13 +50,46 @@ files = []
 labels = []
 file_paths = []
 
-for base in bases_B:
+# fs = 4000.0
+# hrw = 0.05
+# lowcut = 195.0
+# highcut = 882.0
+th = 195.0
+
+# Wavelet Daubechies order 6 (ortogonal)
+# wavelet = pywt.Wavelet('db6')
+# (phi, psi, x) = wavelet.wavefun()
+
+for base in bases_Physiocd:
     instances = []
     files.append(func.get_filenames(path=base, filetype='.wav'))
     # labels.append(func.get_filenames(path=base, filetype='.hea'))
+
     for i in range(len(files[-1])):
+        # instance, rate = func.load_sound_files(base + '/' + files[-1][i])
+        rate, instance = wavfile.read(base + '/' + files[-1][i])
+
+        # filtragem e decomposicao
+        decimateSignal = signal.decimate(instance, 10)
+        coeffs = pywt.wavedec(decimateSignal, 'db6', level=4)  # transformada Wavelet Daubechies order 6
+        cA4, cD4, cD3, cD2, cD1 = coeffs
+
+        cD4 = pywt.threshold(cD4, th, mode='less')
+        cD3 = pywt.threshold(cD3, th, mode='less')
+        cD2 = pywt.threshold(cD2, th, mode='less')
+        cD1 = pywt.threshold(cD1, th, mode='less')
+
+        coeffs = cA4, cD4, cD3, cD2, cD1
+
+        # reconstrucao do sinal
+        recSignal = pywt.waverec(coeffs, 'db6')
+
+        # adicao do valor no vetor
+        instances.append(recSignal)
         print('Done with: ' + files[-1][i])
 
-    func.write_csv(path_B+base[-1], instances)
+    # # func.write_csv(path_B+base[-1], instances)
+    dir = os.path.basename(base)
+    func.write_csv(dir + '_filter', instances)
     print('Done with: ' + base + '\n')
 
